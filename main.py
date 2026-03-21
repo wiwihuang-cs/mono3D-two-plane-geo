5daabb08c89003d4240bb005683d8c5493d1e45a
Wiwihuang <kobeskskk@gmail.com>
2026-03-21 18:21:39 +0800

    chore: add matplotlib when training and ignore the model weight

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from mono3d-occlusion-based!")
+# def main():
+#     print("Hello from mono3d-occlusion-based!")
 
+
+# if __name__ == "__main__":
+#     main()
+# main.py
+from utils.train_road_segmentation import main
 
 if __name__ == "__main__":
     main()
