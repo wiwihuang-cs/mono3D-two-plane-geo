3e76e1f63075cec1c4987e83fd8454dc5fd0a2d2
Wiwi Huang <kobeskskk@gmail.com>
2026-03-21 22:42:54 +0800

    feat: add inference model function

--- libs/model/restnet101.py
+++ libs/model/restnet101.py
@@ -1,7 +1,8 @@
 from torchvision.models.segmentation import deeplabv3_resnet101,  DeepLabV3_ResNet101_Weights
 import torch.nn as nn
+import torch
 
-def build_model(device):
+def build_train_model(device):
     weights = DeepLabV3_ResNet101_Weights.DEFAULT
     model = deeplabv3_resnet101(weights=weights)
 
@@ -12,4 +13,20 @@ def build_model(device):
     )
 
     model.to(device)
+    return model
+
+def build_inference_model(device, model_path):
+    model = deeplabv3_resnet101(weights=None)
+    model.classifier[4] = nn.Conv2d(
+        in_channels=256,
+        out_channels=1,
+        kernel_size=1
+    )
+
+    checkpoint = torch.load(model_path, map_location=device)  # map_location=device is useed to escape GPU/CPU mismatch issues 
+    model.load_state_dict(checkpoint["model"])  # The model is the same as the one used during training, so no need to strict=False
+
+    model.to(device)
+    model.eval()
+
     return model
